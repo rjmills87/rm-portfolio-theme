5bdf7d089d0ef87531a2356525d6699ff9156d2e
Rhys Miller <rjmills87@aol.com>
2024-04-16 22:23:47 +0100

    Amended how content is displayed on project page.

--- project-page.php
+++ project-page.php
@@ -9,36 +9,19 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
         <section class="project">
             <h2 class="project-heading"><?php the_title(); ?></h2>
+
             <div class="project-container">
-                <?php
-                    // Retrieve and display each paragraph of the post content individually
-                    $post_content = get_the_content();
-                    $paragraphs = explode("</p>", $post_content);
-                    foreach ($paragraphs as $paragraph) {
-                        // Strip any remaining HTML tags and trim whitespace
-                        $clean_paragraph = trim(wp_strip_all_tags($paragraph));
-                        // Check if the paragraph is not empty
-                        if (!empty($clean_paragraph)) {
-                            echo "<p class='project-description'>" . $clean_paragraph . "</p>";
-                        }
-                    }
-                    ?>
-                <?php
-
-$icons = get_the_terms(get_the_ID(), 'project_icon');
-
-if ($icons && !is_wp_error($icons)) :
-    ?>
+                <div class="project-description">
+                    <?php the_content(); ?>
+                </div>
+                <?php $icons = get_the_terms(get_the_ID(), 'project_icon'); ?>
+                <?php if ($icons && !is_wp_error($icons)) : ?>
                 <h3 class="tech-heading">Technology Used</h3>
                 <div class="icons">
+                    <?php foreach ($icons as $icon) : ?>
                     <?php
-     
-        foreach ($icons as $icon) :
-            $icon_id = $icon->term_id;
-
-            $icon_svg = get_template_directory_uri() . "/assets/images/" . $icon->name . '.svg';
-            ?>
-
+                            $icon_svg = get_template_directory_uri() . "/assets/images/" . $icon->name . '.svg';
+                            ?>
                     <div class="icon-container">
                         <img class="project-icon" src="<?php echo $icon_svg; ?>" alt="<?php echo $icon->name; ?>" />
                         <p class="icon-name"><?php echo $icon->name; ?></p>
@@ -46,18 +29,17 @@ if ($icons && !is_wp_error($icons)) :
                     <?php endforeach; ?>
                 </div>
                 <?php endif; ?>
-                <img class="project-image" src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>"
-                    alt="<?php the_title(); ?>">
 
+                <?php if (has_post_thumbnail()) : ?>
+                <img class="project-image" src="<?php echo get_the_post_thumbnail_url(); ?>"
+                    alt="<?php the_title(); ?>">
+                <?php endif; ?>
 
                 <div class="btn-container">
                     <?php
-    // Get GitHub Repo Custom Field Value
-    $github_repo = get_post_meta(get_the_ID(), 'github_repo', true);
-   
-    // Get Live Project Custom Field Value
-    $live_project = get_post_meta(get_the_ID(), 'live_project', true);
-    ?>
+                    $github_repo = get_post_meta(get_the_ID(), 'github_repo', true);
+                    $live_project = get_post_meta(get_the_ID(), 'live_project', true);
+                    ?>
                     <?php if (!empty($github_repo)) : ?>
                     <a href="<?php echo esc_url($github_repo); ?>" target="_blank"><button class="link-btn">GitHub
                             Link</button></a>
@@ -68,10 +50,12 @@ if ($icons && !is_wp_error($icons)) :
                     <?php endif; ?>
                 </div>
             </div>
+
             <div class="return-home">
                 <a href="/#projects"><span class="back">Back</span></a>
             </div>
         </section>
     </main>
+</div>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
